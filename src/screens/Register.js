befa9c81a0dd174eca99b99d6d94903d692ad0cd
martinaunamunzaga <69681769+martinaunamunzaga@users.noreply.github.com>
2022-06-11 12:43:30 -0300

    form de registro

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,19 +1,71 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 
 class Register extends Component {
     constructor(props){
         super(props);
         this.state={
+            email: "",
+            password:"",
+            nombreDeUsuario:"",
 
         }
     }
 
+    onSubmit(){
+        console.log(this.state)
+    }
+
     render(){
+        console.log(this.state.email);
+        console.log(this.state.password);
         return(
-            <Text>Register</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Registro</Text>
+                <TextInput
+                    style={styles.field}
+                    keyboardType="default"
+                    placeholder="Nombre de usuario"
+                    onChangeText={ text => this.setState({ nombreDeUsuario: text})}
+                />
+                <TextInput
+                    style={styles.field}
+                    keyboardType="default"
+                    placeholder="Email"
+                    onChangeText={ text => this.setState({ email: text})}
+                />
+                <TextInput
+                    style={styles.field}
+                    keyboardType="default"
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    onChangeText={ text => this.setState({ password: text})}
+                />
+                <TouchableOpacity onPress={()=>this.onSubmit()}>
+                    <Text>Registrarme</Text>
+                    
+                </TouchableOpacity>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:10,
+        marginTop: 10
+    },
+    title:{
+        marginBottom:20
+    },
+    field:{
+        borderColor: '#dcdcdc',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding:3,
+        marginBottom:8
+
+    }
+})
 
 export default Register;

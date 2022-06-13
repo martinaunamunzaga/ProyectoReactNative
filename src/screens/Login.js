3489835ffd4df892c0e37b128ded8f0cf2dbbe04
martinaunamunzaga <69681769+martinaunamunzaga@users.noreply.github.com>
2022-06-13 09:03:15 -0300

    Login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,20 +1,70 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { auth } from "../firebase/config";
 
 class Login extends Component {
     constructor(props){
         super(props);
         this.state={
+            email: "",
+            password:"",
+            
 
         }
     }
 
-    render(){
-        return(
-            <Text>Login</Text>
-        )
+    login(mail, pass){
+        console.log(this.state)
+        auth.signInWithEmailAndPassword(mail, pass)
+           .then(response => console.log(response))
+           .catch(error => console.log(error))
     }
 
+    render(){
+        console.log(this.state.email);
+        console.log(this.state.password);
+        return(
+            <View style={styles.container}>
+                <Text style={styles.title}>Login</Text>
+                
+                <TextInput
+                    style={styles.field}
+                    keyboardType="default"
+                    placeholder="Email"
+                    onChangeText={ text => this.setState({ email: text})}
+                />
+                <TextInput
+                    style={styles.field}
+                    keyboardType="default"
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    onChangeText={ text => this.setState({ password: text})}
+                />
+                <TouchableOpacity onPress={()=>this.login(this.state.email, this.state.password)}>
+                    <Text>Loguearme</Text>
+                    
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:10,
+        marginTop: 10
+    },
+    title:{
+        marginBottom:20
+    },
+    field:{
+        borderColor: '#dcdcdc',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding:3,
+        marginBottom:8
+
+    }
+})
 
 export default Login;

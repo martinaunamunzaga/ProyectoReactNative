3893d6f53f30a1247bb92e1ce7298899199344b1
bauti-saud <bsaud@udesa.edu.ar>
2022-06-16 16:47:43 -0300

    Search Home y post

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,65 @@
-import React, { Component } from "react";
-import { Text } from "react-native";
+import React, {Component} from 'react';
+import { db, auth } from '../firebase/config';
+import { View,
+         Text,
+         TouchableOpacity, 
+         StyleSheet, 
+         ActivityIndicator,
+         FlatList, 
+         Image } from 'react-native';
+import Post from '../components/Post';
 
 class Home extends Component {
     constructor(props){
         super(props);
         this.state={
-
+            posts:[]
         }
     }
+    
+    componentDidMount(){
+        db.collection('posts').orderBy('createdAt', 'desc').onSnapshot(
+            docs => {
+                let posts = [];
+                docs.forEach( oneDoc => {
+                    posts.push({
+                        id: oneDoc.id,
+                        data: oneDoc.data()
+                    })
+                })
+
+                this.setState({
+                    posts: posts
+                })
+            }
+        )
+
+        
+    }
+
 
     render(){
+        // console.log(this.state);
         return(
-          <Text> Home </Text>
+                <View style={styles.container}>
+                    <Text>Posteos</Text>
+                    <FlatList 
+                        data={this.state.posts}
+                        keyExtractor={post => post.id}
+                        renderItem = { ({item}) => <Post dataPost={item} 
+                        {...this.props} />}
+                    />
+                    
+                </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
 
 export default Home;
